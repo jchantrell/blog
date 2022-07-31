dca2a3aea0598e14d0bd2472d7718049fe7d1f8f
Joel Chantrell <joelchantrell@protonmail.com>
2022-07-31 21:38:59 +0930

    move head to component, extend doc for daisy theme

---
+++ src/components/head.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <Head>
+      <title>jchantrell.dev</title>
+      <meta name="description" content="" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
