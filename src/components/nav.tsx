06085484b9c7a6c3322a3422ef794a9e283b30d7
Joel Chantrell <joelchantrell@protonmail.com>
2022-08-06 11:41:30 +0930

    improvements to navbar, added theme changer

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,65 +1,188 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
-function NavBar() {
-  const navigateToExternalUrl = (
-    url: string,
-    shouldOpenNewTab: boolean = true
-  ) =>
-    shouldOpenNewTab
-      ? window.open(url, "_blank")
-      : (window.location.href = url);
+function Title() {
   return (
-    <div className="navbar">
-      <Link href="/">
-        <button className="navbar-start pl-4">jchantrell</button>
+    <Link href="/">
+      <div style={{ cursor: "pointer" }} className="navbar-start pl-4">
+        jchantrell
+      </div>
+    </Link>
+  );
+}
+
+interface NavItemProps {
+  item?: string;
+  url?: string;
+}
+
+function NavItem({ item }: NavItemProps) {
+  return (
+    <Link
+      href={{
+        pathname: item,
+      }}
+    >
+      <a
+        style={{ cursor: "pointer" }}
+        className="btn btn-ghost hidden sm:inline-flex"
+      >
+        {item}
+      </a>
+    </Link>
+  );
+}
+
+function HamburgerItem({ item }: NavItemProps) {
+  return (
+    <li>
+      <Link
+        href={{
+          pathname: item,
+        }}
+      >
+        <a className="btn btn-ghost sm:inline-flex">{item}</a>
       </Link>
-      <div className="navbar-end">
-        <Link href="/projects">
-          <button className="btn btn-ghost hidden sm:inline-flex">
-            Projects
-          </button>
-        </Link>
-        <button
-          className="btn btn-ghost hidden sm:inline-flex"
-          onClick={() => navigateToExternalUrl("https://github.com/jchantrell")}
+    </li>
+  );
+}
+
+function HamburgerMenu() {
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="sm:hidden swap swap-rotate align-end">
+        <input type="checkbox" />
+
+        <svg
+          className="swap-off fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512"
         >
-          Github
-        </button>
-        <Link href="/technology">
-          <button className="btn btn-ghost hidden sm:inline-flex">Tools</button>
-        </Link>
-        <Link href="/contact">
-          <button className="btn btn-ghost hidden sm:inline-flex">
-            Contact
-          </button>
-        </Link>
-        <label className="sm:hidden btn btn-circle swap swap-rotate align-end">
+          <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+        </svg>
+
+        <svg
+          className="swap-on fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512 512"
+        >
+          <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+        </svg>
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <HamburgerItem item="projects" />
+        <HamburgerItem item="technology" />
+        <HamburgerItem item="contact" />
+      </ul>
+    </div>
+  );
+}
+
+function ThemeChanger() {
+  let currentTheme;
+  // if (typeof window !== "undefined") {
+  //   if (window.matchMedia) {
+  //     console.log("matches");
+  //     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  //       console.log("dark");
+  //     } else {
+  //       console.log("light");
+  //     }
+  //   } else {
+  //     console.log("idk");
+  //   }
+  // }
+
+  if (typeof localStorage !== "undefined") {
+    currentTheme = localStorage.getItem("theme");
+  }
+
+  if (currentTheme == "light") {
+    return (
+      <div>
+        <label className="swap swap-rotate">
           <input type="checkbox" />
 
           <svg
-            className="swap-off fill-current"
+            className="swap-on fill-current w-10 h-10"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
+            viewBox="0 0 24 24"
+            data-set-theme="dark"
           >
-            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
 
           <svg
-            className="swap-on fill-current"
+            className="swap-off fill-current w-10 h-10"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
+            viewBox="0 0 24 24"
+            data-set-theme="light"
           >
-            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
       </div>
+    );
+  } else
+    return (
+      <div>
+        <label className="swap swap-rotate">
+          <input type="checkbox" defaultChecked />
+
+          <svg
+            className="swap-on fill-current w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            data-set-theme="dark"
+          >
+            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+          </svg>
+
+          <svg
+            className="swap-off fill-current w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            data-set-theme="light"
+          >
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+          </svg>
+        </label>
+      </div>
+    );
+}
+
+function NavBar() {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+  return (
+    <div className="navbar">
+      <Title />
+      <div className="navbar-end">
+        <NavItem item="projects" />
+        <NavItem item="technology" />
+        <NavItem item="contact" />
+        <HamburgerMenu />
+        <ThemeChanger />
+      </div>
     </div>
   );
+}
+
+{
+  /* <Link href="https://github.com/jchantrell">
+  <a target="_blank" rel="noopener noreferrer" className="link">
+    github
+  </a>
+</Link>; */
 }
 
 export default NavBar;
