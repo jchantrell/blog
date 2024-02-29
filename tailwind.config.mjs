7568b2b39afc5664d1f19de3345ad518bc4daa12
Joel <joelchantrell@protonmail.com>
2024-02-29 10:52:58 +1030

    remove redundant tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-      color: {
-        primary: "#548E9B",
-        "text-primary": "#FFF",
-        "text-secondary": "#CCC",
-        background: "#202122"
-    }
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [],
+};
