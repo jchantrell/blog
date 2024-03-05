68da9d6ea81d5f9c212ddfc673d8bd5566c362cf
Joel <joelchantrell@protonmail.com>
2024-03-06 08:17:46 +1030

    add transform helper to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,25 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.inset-center': {
+          top: '50%',
+          left: '50%',
+          '@apply -translate-x-1/2 -translate-y-1/2': {},
+        },
+        '.inset-y-center': {
+          top: '50%',
+          '@apply -translate-y-1/2': {},
+        },
+        '.inset-x-center': {
+          left: '50%',
+          '@apply -translate-x-1/2': {},
+        },
+      });
+    }),
+  ],
 };
