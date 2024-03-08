6d6952a8fc72e3de26a5f22cc81e78406576fedb
Joel <joelchantrell@protonmail.com>
2024-03-08 19:53:59 +1030

    remove svelte from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
 import rehypeExternalLinks from 'rehype-external-links';
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
-import node from "@astrojs/node";
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte(), tailwind(), solidJs()],
+  integrations: [mdx(), tailwind(), solidJs()],
   markdown: {
     shikiConfig: {
-      theme: 'nord'
+      theme: 'nord',
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: '_blank'
-    }]]
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+        },
+      ],
+    ],
   },
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
+    mode: 'standalone',
+  }),
 });
