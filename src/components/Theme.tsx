c0d908464f501eb69f6942d6555e24a71b649a7c
Joel Chantrell <me@jchantrell.dev>
2025-06-08 17:04:32 +0930

    style: button should appear clickable

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -36,6 +36,7 @@ function LightIcon(props: { setTheme: Setter<string> }) {
       height='20'
       viewBox='0 0 20 20'
       fill='currentColor'
+      class='cursor-pointer'
     >
       <path
         fill-rule='evenodd'
@@ -55,6 +56,7 @@ function DarkIcon(props: { setTheme: Setter<string> }) {
       height='20'
       viewBox='0 0 20 20'
       fill='currentColor'
+      class='cursor-pointer'
     >
       <path d='M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z' />
     </svg>
